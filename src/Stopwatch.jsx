236e20e67c1bdbb93864da4d82c04093a6454b24
amr <amrmasri999@gmail.com>
2024-06-13 14:33:20 +0300

    fsgdfgfdgdfkrfr

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -5,6 +5,12 @@ function Stopwatch(){
 
     const [isRunning, setIsRunning] = useState(false);
     const [elapsedTime, setElapsedTime] = useState(0);
+    const intervalIdRef = useRef (null);
+    const startTimeRef = useRef(0);
+
+    useEffect(() => {
+
+    }, [isRunning])
 
     return(<Stopwatch />);
 }
