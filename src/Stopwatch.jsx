6d5332af37b21bbda05365aef1894c2eb5468941
amr <amrmasri999@gmail.com>
2024-06-13 14:28:18 +0300

    sddfzd

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -3,6 +3,9 @@ import React, {useState, useEffect, useRef} from 'react';
 
 function Stopwatch(){
 
-    return(<></>);
+    const [isRunning, setIsRunning] = useState(false);
+    const [elapsedTime, setElapsedTime] = useState(0);
+
+    return(<Stopwatch />);
 }
 export default Stopwatch
